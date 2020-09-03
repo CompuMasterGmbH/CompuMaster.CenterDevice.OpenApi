0696d2ab34419c9638826ef3097a322248a002f8
Jochen Wezel <jwezel@compumaster.de>
2020-09-03 21:26:29 +0200

    Updated version infos

---
+++ CenterDevice.Model/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CenterDevice.Model")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CenterDevice.Model")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020 CenterDevice GmbH, Bonn/Germany and CompuMaster GmbH, Emmelshausen/Germany")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c16f46d8-461b-4fe3-b8d6-26b3dbe6fa6c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CenterDevice.Rest/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CenterDevice.Rest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CenterDevice.Rest")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020 CenterDevice GmbH, Bonn/Germany and CompuMaster GmbH, Emmelshausen/Germany")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6fdbda8a-ee11-46cb-b416-759f0800420b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
