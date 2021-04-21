5599b3c92c8c5f63049393b9077803630403ba85
Jochen Wezel <jwezel@compumaster.de>
2021-04-21 15:37:04 +0200

    fixed compile errors

--- nuget-build-and-push.cmd
+++
@@ -1,3 +0,0 @@
-::REM -UpdateNuGetExecutable not required since it's updated by VS.NET mechanisms
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'CenterDevice.Rest\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\CenterDevice.Rest\CenterDevice.Rest.csproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
-pause
